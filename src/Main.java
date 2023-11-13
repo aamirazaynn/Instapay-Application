b32cd216f9754f7c361fd9c33624fa1650630df0
Amira Zayn <amirazayn5@gmail.com>
2023-11-13 23:23:44 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -73,8 +73,7 @@ public class Main {
                 }
                     break;
                 // --------------------------------------sign in -----------------------------------------------
-                case '2':
-                {
+                case '2': {
                     SignIn signIn = new SignIn();
                     System.out.print("Enter your username: ");
                     String userName = scanner.next();
@@ -82,81 +81,96 @@ public class Main {
                     System.out.print("Enter your password: ");
                     String password = scanner.next();
                     signIn.setPassword(password);
-                    if (signIn.confirmSignIn(applicationData)) {
+                    if(signIn.confirmSignIn(applicationData)){
                         System.out.println("Logged in successfully");
-                        User loggedInUser = applicationData.getUser(userName);
-                        System.out.print("1. Transfer, 2. PayBills, 3. Logout: ");
-                        char operation = scanner.next().charAt(0);
-                        UserController userController = new UserController();
-                        //----------------------------------------operations-----------------------------------------
-                        switch (operation) {
-                            // -----------------------------------transfer money------------------------------------
-                            case '1':{
-                                if (loggedInUser.getUsertype() == Usertype.WALLET) {
-                                    System.out.print("1. Wallet, 2.InstaPay Account: ");
-                                } else if (loggedInUser.getUsertype() == Usertype.BANK) {
-                                    System.out.print("1. Wallet, 2.InstaPay Account, 3. Bank: ");
-                                }
-                                char transferType = scanner.next().charAt(0);
-                                float amount;
-                                String toUser;
-                                switch (transferType) {
-                                    // ------------------------ to wallet --------------------------------------------
-                                    case '1':{
-                                        transaction = new WalletTransaction();
-                                        userController.setTransaction(transaction);
-                                        System.out.print("Enter the amount you want to transfer: ");
-                                        amount = scanner.nextFloat();
-                                        System.out.print("Enter the phone number you want to transfer to: ");
-                                        toUser = scanner.next();
-                                        walletProvider.printUsers();
-                                        System.out.println("------------------------------------------");
-                                        userController.transferMoney(loggedInUser, amount, toUser, "VODAFONE", applicationData);
-                                        walletProvider.printUsers();
+                    }
+                    boolean loop = true;
+                    while(loop) {
+                        if (signIn.confirmSignIn(applicationData)) {
+                            User loggedInUser = applicationData.getUser(userName);
+                            System.out.print("1. Transfer, 2. PayBills, 3. Logout: ");
+                            char operation = scanner.next().charAt(0);
+                            UserController userController = new UserController();
+                            //----------------------------------------operations----------------------------------------
+                            switch (operation) {
+                                // -----------------------------------transfer money------------------------------------
+                                case '1':{
+                                    if (loggedInUser.getUsertype() == Usertype.WALLET) {
+                                        System.out.print("1. Wallet, 2.InstaPay Account: ");
+                                    } else if (loggedInUser.getUsertype() == Usertype.BANK) {
+                                        System.out.print("1. Wallet, 2.InstaPay Account, 3. Bank: ");
                                     }
-                                    break;
-                                    // -------------------------- to instapay ---------------------------------------
-                                    case '2': {
-                                        transaction = new InstapayTransaction();
-                                        userController.setTransaction(transaction);
-                                        System.out.print("Enter the amount you want to transfer: ");
-                                        amount = scanner.nextFloat();
-                                        System.out.print("Enter the username you want to transfer to: ");
-                                        toUser = scanner.next();
-                                        userController.transferMoney(loggedInUser, amount, toUser, "INSTAPAY", applicationData);
-                                    }
-                                    break;
-                                    // ---------------------------- to bank ---------------------------------------
-                                    case '3': {
-                                        transaction = new BankTransaction();
-                                        userController.setTransaction(transaction);
-                                        System.out.print("Enter the amount you want to transfer: ");
-                                        amount = scanner.nextFloat();
-                                        System.out.print("Enter the phone number you want to transfer to: ");
-                                        toUser = scanner.next();
-                                        System.out.print("Which bank you want to transfer to, 1. CIB, 2. National Bank of Egypt: ");
-                                        char bankChoice = scanner.next().charAt(0);
-                                        switch (bankChoice) {
-                                            case '1': {
-                                                cibProvider.printUsers();
-                                                System.out.println("------------------------------------------");
-                                                userController.transferMoney(loggedInUser, amount, toUser, "CIB", applicationData);
-                                                cibProvider.printUsers();
-                                            }
-                                                break;
-                                            case '2': {
-                                                nationalBankOfEgyptProvider.printUsers();
-                                                System.out.println("------------------------------------------");
-                                                userController.transferMoney(loggedInUser, amount, toUser, "NATIONALBANKOFEGYPT", applicationData);
-                                                nationalBankOfEgyptProvider.printUsers();
-                                            }
-                                                break;
+                                    char transferType = scanner.next().charAt(0);
+                                    float amount;
+                                    String toUser;
+                                    switch (transferType) {
+                                        // ------------------------ to wallet --------------------------------------------
+                                        case '1':{
+                                            transaction = new WalletTransaction();
+                                            userController.setTransaction(transaction);
+                                            System.out.print("Enter the amount you want to transfer: ");
+                                            amount = scanner.nextFloat();
+                                            System.out.print("Enter the phone number you want to transfer to: ");
+                                            toUser = scanner.next();
+                                            walletProvider.printUsers();
+                                            System.out.println("------------------------------------------");
+                                            userController.transferMoney(loggedInUser, amount, toUser, "VODAFONE", applicationData);
+                                            walletProvider.printUsers();
                                         }
+                                        break;
+                                        // -------------------------- to instapay ---------------------------------------
+                                        case '2': {
+                                            transaction = new InstapayTransaction();
+                                            userController.setTransaction(transaction);
+                                            System.out.print("Enter the amount you want to transfer: ");
+                                            amount = scanner.nextFloat();
+                                            System.out.print("Enter the username you want to transfer to: ");
+                                            toUser = scanner.next();
+                                            userController.transferMoney(loggedInUser, amount, toUser, "INSTAPAY", applicationData);
+                                        }
+                                        break;
+                                        // ---------------------------- to bank ---------------------------------------
+                                        case '3': {
+                                            transaction = new BankTransaction();
+                                            userController.setTransaction(transaction);
+                                            System.out.print("Enter the amount you want to transfer: ");
+                                            amount = scanner.nextFloat();
+                                            System.out.print("Enter the phone number you want to transfer to: ");
+                                            toUser = scanner.next();
+                                            System.out.print("Which bank you want to transfer to, 1. CIB, 2. National Bank of Egypt: ");
+                                            char bankChoice = scanner.next().charAt(0);
+                                            switch (bankChoice) {
+                                                case '1': {
+                                                    cibProvider.printUsers();
+                                                    System.out.println("------------------------------------------");
+                                                    userController.transferMoney(loggedInUser, amount, toUser, "CIB", applicationData);
+                                                    cibProvider.printUsers();
+                                                }
+                                                break;
+                                                case '2': {
+                                                    nationalBankOfEgyptProvider.printUsers();
+                                                    System.out.println("------------------------------------------");
+                                                    userController.transferMoney(loggedInUser, amount, toUser, "NATIONALBANKOFEGYPT", applicationData);
+                                                    nationalBankOfEgyptProvider.printUsers();
+                                                }
+                                                break;
+                                            }
+                                        }
+                                        break;
                                     }
-                                    break;
                                 }
-                            }
                                 break;
+                                // ------------------------------------pay bills----------------------------------------
+                                case '2': {
+
+                                }
+                                break;
+                                // -----------------------------------logout-------------------------------------------
+                                case '3': {
+                                    loop = false;
+                                }
+                                break;
+                            }
                         }
                     }
                 }
